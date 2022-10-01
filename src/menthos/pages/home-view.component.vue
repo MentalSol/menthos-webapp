63d78c28fdf862c19983427de35b086e2fdded3d
Ryllex <epostigo1999@gmail.com>
2022-10-01 01:31:10 -0500

    feat: Home-view.component updated

--- src/menthos/pages/home-view.component.vue
+++ src/menthos/pages/home-view.component.vue
@@ -1,6 +1,14 @@
 <template>
-  <h1>XDXDXDX</h1>
+  <div class="grid ">
+    <div class="col-fixed border-3" style="width:300px">
+      <div class="block">
+        <h1>Asignaturas</h1>
+      </div>
 
+    </div>
+    <div class="col border-3">2</div>
+    <div class="col-fixed border-3" style="width:400px">3</div>
+  </div>
 </template>
 
 <script>
