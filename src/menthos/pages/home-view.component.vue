4948d44d183229f40f0ed0cfb0cae5ca347dead6
Ryllex <epostigo1999@gmail.com>
2022-10-22 20:02:41 -0500

    feat:Home view implemented

--- src/menthos/pages/home-view.component.vue
+++ src/menthos/pages/home-view.component.vue
@@ -6,19 +6,49 @@
       </div>
        <div >
           <div v-for="subject in subjects" class="block">
-            <h4>{{subject.title}}</h4>
+           {{subject.image}} <h4>{{subject.title}}</h4>
           </div>
        </div>
 
     </div>
     <div class="col border-3">
-      <div class=" justify-content-center align-items-center">
+      <div class="flex align-items-center justify-content-center ">
         <h1>Ey! Soy BotBrain</h1>
         <h1>¿Qué es lo que quieres hacer?</h1>
       </div>
-
+         <div v-for="question in questions" class="block">
+           <pv-card >
+             <template #header>
+               <h1>Question #{{question.id}}</h1>
+             </template>
+             <template #title>
+               {{question.content}}
+             </template>
+           </pv-card>
+         </div>
     </div>
     <div class="col-fixed border-3" style="width:400px">
+      <div v-for="user in users" class="block">
+        <pv-card>
+          <template #header >
+            <img
+                class="user-img-0"
+                alt="user header"
+                style="border-radius: 3%"
+                :src="user.photo"
+            />
+          </template>
+          <template #title>
+            {{user.name}}
+          </template>
+          <template #content>
+           Respuestas:{{user.answers}}<br>
+            Puntos:{{user.points}}<br>
+            Carrera:{{user.career}}<br>
+            Puesto Ranking:{{user.Ranking}}
+          </template>
+        </pv-card>
+      </div>
 
     </div>
   </div>
@@ -28,6 +58,7 @@
 <script>
 import {SubjectsApiServices} from "@/menthos/services/subject/subjects-api.services";
 import {UsersApiServices} from "@/menthos/services/user/users-api.services";
+import {QuestionsApiServices} from "@/menthos/services/question/questions-api.services";
 
 export default {
   name: "home-view.component",
@@ -38,14 +69,20 @@ export default {
     return{
       subjects: [],
       subjectsService: null,
-      subject:{},
-      submitted: false,
-      tutorialDialog: false
+      users:[],
+      usersService: null,
+      questions:[],
+      questionsService:null,
+
     }
   },
   created() {
     this.subjectsService= new SubjectsApiServices();
+    this.usersService = new UsersApiServices();
+    this.questionsService = new QuestionsApiServices();
     this.getSubject();
+    this.getUser();
+    this.getQuestion();
   },
   methods:{
     getSubject(){
@@ -54,10 +91,17 @@ export default {
         console.log(this.subjects);
       });
     },
-    openNew() {
-      this.tutorial = {};
-      this.submitted = false;
-      this.tutorialDialog = true;
+    getUser(){
+      this.usersService.getUsers().then((response)=>{
+        this.users=response.data;
+        console.log(this.users);
+      });
+    },
+    getQuestion(){
+      this.questionsService.getQuestions().then((response)=>{
+        this.questions=response.data;
+        console.log(this.questions);
+      });
     },
   },
 }
---
+++ src/menthos/services/question/questions-api.services.js
@@ -0,0 +1,19 @@
+import http from "../../../core/services/http-common";
+
+export class QuestionsApiServices {
+    //GET
+    getQuestions() {
+        return http.get("/questions");
+    }
+    //POST
+    createQuestions(data) {
+        return http.post("/questions", data);
+    }
+    //GET BY ID
+    getQuestionById(id) {
+        return http.get(`/questions/${id}`);
+    }
+    updateQuestion(id, data) {
+        return http.put(`/questions/${id}`, data);
+    }
+}
